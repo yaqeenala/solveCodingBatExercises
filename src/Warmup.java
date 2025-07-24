62db6dd7ff3ca7f001c83bb5f83800b3bb695df0
Yaqeen <yaqeen.alabdali@student.manchester.ac.uk>
2025-07-24 12:23:30 +0400

    Add solution for warmup1-<problemName>

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,35 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Warmup {
-    public static boolean sleepIn(boolean weekday, boolean vacation) {
-        if (weekday == false || vacation == true) {
+    public static boolean makes10(int a, int b) {
+        int c = a + b;
+        if (a == 10 || b == 10 || c == 10){
             return true;
         }
         return false;
-    }
-
-    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        if ((aSmile == true && bSmile == true) || (aSmile == false && bSmile == false)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static int sumDouble(int a, int b) {
-        int sum = 0;
-        if (a != b) {
-            sum = a + b;
-        } else if (a == b) {
-            sum = 2 * (a + b);
-        }
-        return sum;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(sleepIn(false, true));
-        System.out.println(monkeyTrouble(false, false));
-        System.out.println(sumDouble(5, 5));
+        System.out.println(makes10(5, 5));
+
 
 
     }
