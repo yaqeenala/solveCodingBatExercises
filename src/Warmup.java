6e734868e6ada6ac3c7e9255056b8e062d4dc46d
Yaqeen <yaqeen.alabdali@student.manchester.ac.uk>
2025-07-24 09:29:38 +0400

    sleepIn method is added

---
+++ src/Warmup.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Warmup {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday == false || vacation == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, true));
+    }
+}
