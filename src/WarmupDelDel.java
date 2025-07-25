49ac95574a9f9da2b03f0d5af3b717eea5009744
Yaqeen <yaqeen.alabdali@student.manchester.ac.uk>
2025-07-25 16:34:12 +0400

    Add solution for warmup1-delDel

--- src/WarmupDelDel.java
+++ src/WarmupDelDel.java
@@ -1,4 +1,4 @@
-public class WarmupLoneTeen {
+public class WarmupDelDel {
     public static String delDel(String str) {
         if (str.length()>=4 && str.substring(1, 4).equals("del")) {
             return str.substring(0, 1) + str.substring(4);
